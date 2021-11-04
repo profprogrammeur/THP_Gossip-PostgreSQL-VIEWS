f5c0fc9754ff52ff61f6e81aa94528af67ca0302
profprogrammeur <contact@profprogrammeur.com>
2021-11-04 23:59:37 +0100

    autenticate first

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,44 +1,48 @@
 class GossipsController < ApplicationController
+
+  before_action :authenticate_user, only: [:show, :new, :edit, :update, :create, :destroy]
+
+
   def index
     @gossips=Gossip.all
     @users = User.all
   end
 
   def show
-    @gossip = Gossip.find(params[:id])
-    @user = User.find(@gossip.user_id)
+      @gossip = Gossip.find(params[:id])
+      @user = User.find(@gossip.user_id)
   end
   
   def new
-    puts"////////////////////////////////////////////////"
-    puts "GossipController => new"
-    puts params
-        puts"////////////////////////////////////////////////"
-        # @gossip = Gossip
         @gossip=Gossip.new
-
   end
 
   def create
-     puts"////////////////////////////////////////////////"
-    puts "GossipController => create"
-    puts params
-     puts"////////////////////////////////////////////////"
-     @gossip= Gossip.new(title: params[:title], content: params[:content], user: User.find(3))
+     @gossip= Gossip.new(title: params[:title], content: params[:content], user: current_user)
       
-     if @gossip.save
-       redirect_to gossip_path(@gossip.id)
-      flash[:success] = "Bonne création"
-     else
-        render 'new'
-     end
+      if @gossip.save
+        redirect_to gossip_path(@gossip.id)
+        flash[:success] = "Bonne création"
+      else
+          render 'new'
+      end
+   
      #  @gossip = Gossip.create(gossip_params)
     #  redirect_to gossips
+    
      
     
   end
 
-  # private
+  private
+
+  def authenticate_user
+        unless session_en_cours?
+          flash[:danger] = "Merci de vous connecter"
+          redirect_to new_session_path
+      end
+    end 
+
   #   def gossip_params
   #     params.require(:gossip).permit(:title, :content)
   #   end
