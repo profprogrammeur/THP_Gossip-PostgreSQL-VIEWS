22d0c2412e06c8569356ba9fb9ca42efb9144b54
profprogrammeur <contact@profprogrammeur.com>
2021-11-05 00:49:22 +0100

    login before anything

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -2,6 +2,7 @@ class GossipsController < ApplicationController
 
   before_action :authenticate_user, only: [:show, :new, :edit, :update, :create, :destroy]
 
+  before_action :good_user, only: [:edit, :update, :destroy]
 
   def index
     @gossips=Gossip.all
@@ -17,22 +18,35 @@ class GossipsController < ApplicationController
         @gossip=Gossip.new
   end
 
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    gossip_params = params.require(:gossip).permit(:title, :content)
+    @gossip.update(gossip_params)
+    redirect_to action: "show"
+  end
+
   def create
      @gossip= Gossip.new(title: params[:title], content: params[:content], user: current_user)
-      
       if @gossip.save
         redirect_to gossip_path(@gossip.id)
         flash[:success] = "Bonne création"
       else
           render 'new'
       end
-   
-     #  @gossip = Gossip.create(gossip_params)
-    #  redirect_to gossips
-    
-     
-    
   end
+
+  def destroy
+     @gossip = Gossip.find(params[:id])
+     puts "*" * 60
+     @gossip.destroy
+     puts "*" * 60
+     redirect_to :action => "index"
+  end
+
 
   private
 
@@ -43,8 +57,18 @@ class GossipsController < ApplicationController
       end
     end 
 
-  #   def gossip_params
-  #     params.require(:gossip).permit(:title, :content)
-  #   end
+    def gossip_params
+      params.require(:gossip).permit(:title, :content)
+    end
+
+    def good_user
+        if is_owner?(Gossip.find(params[:id]).user_id)
+          flash[:success] = "Popotin bien modifier/supprimer"
+
+        else flash[:danger] = "T'as pas le droit? T'as pas le droit. T'as pas le droiaaaaaaaaa!!!"
+          redirect_to gossips_path
+      end
+    end 
+
 
 end
