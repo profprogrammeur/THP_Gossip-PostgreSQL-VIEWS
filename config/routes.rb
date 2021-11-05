36355e4cb546a18dfacb4783b3719ba1cb835cba
profprogrammeur <contact@profprogrammeur.com>
2021-11-05 19:22:28 +0100

    comment Team and Contact , get route and app.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  root "gossips#index"
+
+root "gossips#index"
 
 resources :sessions, only: [:new, :create, :destroy]
 resources :users
 resources :cities, only: [:show]
 resources :gossips
-get '/team', to: 'static_pages#team'
-get '/contact', to: 'static_pages#contact'
+# get '/team', to: 'static_pages#team'
+# get '/contact', to: 'static_pages#contact'
 
 
 end
